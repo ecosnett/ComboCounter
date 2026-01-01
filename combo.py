d88e8c3a4f3c22ac23ce62052713db1d172c7b5b
edcos12 <edwardcosnett@gmail.com>
2026-01-01 07:32:33 +0000

    adding interval calculation

--- combo.py
+++ combo.py
@@ -8,37 +8,34 @@ hits = {"jab": 1, "cross": 2, "look": 2, "rook": 2, "lut": 2, "rut": 2, "lunder"
 right = ["cross", "rook", "rut", "runder", "rip"]
 left = ["jab", "look", "lut", "lunder", "lip"]
 intervals = {1:0.0, 2:0.5, 3:1}
-keys = list(intervals.keys())
-intWeights = [1/k for k in keys]
-length = ""
 current = []
 used = []
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-@app.route('/')
-def homePage():
-     return render_template('FrontPage.html')
+# @app.route('/')
+# def homePage():
+#      return render_template('FrontPage.html')
 
-@app.route('/startCountdown', methods=['POST'])
-def startCountdown():
-  try:
-      global length
-      length = request.form.get('length', '').strip()
-      if not length:
-            return "Give a valid option"
-      return redirect(url_for("countdown"))
-  except Exception as e:
-      return f"An error occurred: {str(e)}", 500   
+# @app.route('/startCountdown', methods=['POST'])
+# def startCountdown():
+#   try:
+#       global length
+#       length = request.form.get('length', '').strip()
+#       if not length:
+#             return "Give a valid option"
+#       return redirect(url_for("countdown"))
+#   except Exception as e:
+#       return f"An error occurred: {str(e)}", 500   
 
-@app.route("/countdown")
-def countdown():
-    return render_template("Countdown.html", seconds=3)
+# @app.route("/countdown")
+# def countdown():
+#     return render_template("Countdown.html", seconds=3)
 
-@app.route("/startCombo", methods=["POST"])
-def start():
-    callout()
-    return jsonify(success=True)
+# @app.route("/startCombo", methods=["POST"])
+# def start():
+#     callout()
+#     return jsonify(success=True)
 
 
 def speak(voice):
@@ -50,37 +47,24 @@ def speak(voice):
 
   engine.runAndWait()
 
-def random_hits():
-        hit = rd.choice(hits, replace=True)
-        return hit
-
-def callout():
-    for i in random_hits():
-        interval = rd.choices(keys, weights=intWeights, k=1)[0]
-        speak(i)
-        print("Move: " + i + "\n" + "Interval: " + str(intervals[interval]) + "s")
-        time.sleep(intervals[interval])
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
-
 def IntervalCalc():
     interval = intervals[hits[current[0]]]
     if (current[0] in left and current[1] in left) or (current[0] in right and current[1] in right):
-        return interval
+        return interval+0.5
 
-    return interval +1
+    return interval
 
-def Start():
-    if not bool(current):
-        current.append(random_hits)
+def Start(length):
+    while len(used) < length:
+        if not bool(current):
+            current.append(rd.choice(list(hits.keys())))
 
-    current.append(random_hits)
-    WaitTime = IntervalCalc()
-    speak(current[0])
-    time.sleep(WaitTime)
+        current.append(rd.choice(list(hits.keys())))
+        WaitTime = IntervalCalc()
+        used.append(current[0])
+        current.pop(0)
+        speak(current[0])
+        print(f"action:{current[0]}\ninterval:{WaitTime}")
+        time.sleep(WaitTime)
 
-    
-
-
+Start(10)
